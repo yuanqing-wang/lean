593463f23782b1ef7fe39e9f12a93c95e513467f
Yuanqing Wang <wangyq@wangyq.net>
2024-07-08 17:41:51 +0800

    reverse

--- lean/samplers.py
+++ lean/samplers.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
-from typing import Callable, NamedTuple
+from typing import Callable, NamedTuple, Optional
+from functools import partial
 
 class HamiltonianMonteCarlo(NamedTuple):
     """Hamiltonian Monte Carlo algorithm.
@@ -24,7 +25,13 @@ class HamiltonianMonteCarlo(NamedTuple):
     step_size: float
     steps: int
 
-    def step(self, position: jnp.ndarray, momentum: jnp.ndarray):
+    def step(
+            self, 
+            position: jnp.ndarray, 
+            momentum: jnp.ndarray,
+            step: Optional[int] = None,
+            reverse: bool = False
+        ):
         """Run the Hamiltonian Monte Carlo algorithm.
 
         Parameters
@@ -35,21 +42,32 @@ class HamiltonianMonteCarlo(NamedTuple):
         momentum : jnp.ndarray
             Initial momentum.
         """
+        # compute time
+        if step is not None:
+            time = (step / self.steps)
+        else:
+            time = 0.0
+
+        step_size = self.step_size * (-1 if reverse else 1)
+
+        # compose potential energy
+        potential = partial(self.potential, time=time)
+
         # compute force
-        force = -jax.grad(self.potential)(position)
+        force = -jax.grad(potential)(position)
 
         # leapfrog integration
         # update momentum
-        momentum = momentum - 0.5 * self.step_size * force
+        momentum = momentum + 0.5 * step_size * force
 
         # update position
-        position = position + self.step_size * momentum
+        position = position + step_size * momentum
 
         # update force
-        force = -jax.grad(self.potential)(position)
+        force = -jax.grad(potential)(position)
 
         # update momentum
-        momentum = momentum - 0.5 * self.step_size * force
+        momentum = momentum + 0.5 * step_size * force
 
         return position, momentum
     
@@ -73,15 +91,44 @@ class HamiltonianMonteCarlo(NamedTuple):
 
         # run leapfrog integration
         state = jax.lax.fori_loop(
-            0, self.steps, lambda i, state: self.step(*state), state
+            0, self.steps, lambda i, state: self.step(*state, step=i), state
         )
 
         # unpack
         position, momentum = state
         return position, momentum
+    
+    def reverse(
+            self,
+            position: jnp.ndarray,
+            momentum: jnp.ndarray,
+    ):
+        """Run the Hamiltonian Monte Carlo algorithm in reverse.
 
-        
+        Parameters
+        ----------
+        position : jnp.ndarray
+            Initial position.
 
+        momentum : jnp.ndarray
+            Initial momentum.
+
+        schedule : jnp.ndarray
+            Schedule for the step size.
+        """
+        # initialize state
+        state = (position, momentum)
+
+        # run leapfrog integration
+        state = jax.lax.fori_loop(
+            0, self.steps,
+            lambda i, state: self.step(*state, step=self.steps-i-1, reverse=True), 
+            state,
+        )
+
+        # unpack
+        position, momentum = state
+        return position, momentum
 
 
 
--- lean/tests/test_samplers.py
+++ lean/tests/test_samplers.py
@@ -3,9 +3,41 @@ def test_hmc():
     import jax.numpy as jnp
     position = jax.random.normal(jax.random.PRNGKey(0), (100,))
     momentum = jax.random.normal(jax.random.PRNGKey(0), (100,))
-    potential = lambda x: 0.5 * jnp.sum(x ** 2)
+    potential = lambda x, time: 0.5 * jnp.sum(x ** 2)
     kinetic = lambda x: 0.5 * jnp.sum(x ** 2)
     from lean.samplers import HamiltonianMonteCarlo
     sampler = HamiltonianMonteCarlo(potential, kinetic, 0.1, 10)
     position, momentum = sampler(position, momentum)
-    
+
+def test_constant_potential_reverse():
+    import jax
+    import jax.numpy as jnp
+    position0 = jax.random.normal(jax.random.PRNGKey(0), (100,))
+    momentum0 = jax.random.normal(jax.random.PRNGKey(0), (100,))
+    potential = lambda x, time: 0.5 * jnp.sum(x ** 2)
+    kinetic = lambda x: 0.5 * jnp.sum(x ** 2)
+    from lean.samplers import HamiltonianMonteCarlo
+    sampler = HamiltonianMonteCarlo(potential, kinetic, 0.1, 10)
+    position1, momentum1 = sampler(position0, momentum0)
+
+    position2, momentum2 = sampler.reverse(position1, momentum1)
+    assert jnp.allclose(position0, position2)
+    assert jnp.allclose(momentum0, momentum2)
+
+def test_time_dependent_potential_reverse():
+    import jax
+    import jax.numpy as jnp
+    position0 = jax.random.normal(jax.random.PRNGKey(0), (100,))
+    momentum0 = jax.random.normal(jax.random.PRNGKey(0), (100,))
+    potential = lambda x, time: 0.5 * jnp.sum(time * x ** 2)
+    kinetic = lambda x: 0.5 * jnp.sum(x ** 2)
+    from lean.samplers import HamiltonianMonteCarlo
+    sampler = HamiltonianMonteCarlo(potential, kinetic, 0.1, 10)
+    position1, momentum1 = sampler(position0, momentum0)
+
+    position2, momentum2 = sampler.reverse(position1, momentum1)
+    assert jnp.allclose(position0, position2)
+    assert jnp.allclose(momentum0, momentum2)
+
+
+
