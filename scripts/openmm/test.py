aafd0a9267bfaba6f527e65852305f719859783a
Yuanqing Wang <wangyq@wangyq.net>
2024-09-10 16:41:40 -0400

    openmm test

--- scripts/openmm/test.py
+++ scripts/openmm/test.py
@@ -7,6 +7,8 @@ import openmm as mm
 import numpy as np
 import torch
 import math
+import copy
+from multiprocessing import Pool
 
 class CenteredNormal(torch.nn.Module):
     def __init__(self, log_sigma, particles, dimension=3):
@@ -55,18 +57,50 @@ def dw2(batch_size=1):
         force.addBond(2*idx, 2*idx+1, [-2.0, 0.45, 4.0])
     system.addForce(force)
     return system
+
+def perturb(original_parameters, coefficients, system, context):
+    force = system.getForce(0)
+    for idx in range(force.getNumBonds()):
+        parameters = [coefficients["k2"], coefficients["k4"], 4.0]
+        _, __, _original_parameters = original_parameters[idx]
+        parameters = [_original_parameters[0] * parameters[0], _original_parameters[1] * parameters[1], 4.0]
+        force.setBondParameters(idx, 2*idx, 2*idx+1, parameters)
+    force.updateParametersInContext(context)
+    force = system.getForce(1)
+    force.setGlobalParameterDefaultValue(0, 1-coefficients["k_gaussian"])
+    force.updateParametersInContext(context)
+    
+def add_umbrella(system):
+    force = mm.CustomExternalForce("k*((x/sigma)^2+(y/sigma)^2+(z/sigma)^2)")
+    force.addGlobalParameter("k", -0.5)
+    force.addGlobalParameter("sigma", 1.0)
+    system.addForce(force)
+    return force
+    
+def _get_original(system):
+    force = system.getForce(0)
+    original_parameters = []
+    for idx in range(force.getNumBonds()):
+        original_parameters.append(force.getBondParameters(idx))
+    return original_parameters
+
+def restore(system, original_parameters):
+    force = system.getForce(0)
+    for idx in range(force.getNumBonds()):
+        force.setBondParameters(idx, *original_parameters[idx])
+    force = system.getForce(1)
+    force.setGlobalParameterDefaultValue(0, 1.0)
         
 class Policy(torch.nn.Module):
     def __init__(
         self,
         system: mm.System,
-        integrator: mm.Integrator,
+        batch_size: int = 10,
     ):
         super().__init__()
+        self.batch_size = batch_size
         self.centered_normal = CenteredNormal(torch.tensor(0.0), 2)
         self.system = system
-        self._get_original()
-        self._add_unbrella()
         self.schedules = torch.nn.ModuleDict(
             {
                 "k2": MeanFieldSinRBFSchedule(10),
@@ -74,29 +108,7 @@ class Policy(torch.nn.Module):
                 "k_gaussian": MeanFieldSinRBFSchedule(10),
             }
         )
-        self.integrator = integrator
-        self.context = mm.Context(system, integrator, mm.Platform.getPlatformByName('Reference'))
-        
-        
-    def _get_original(self):
-        force = self.system.getForce(0)
-        original_parameters = []
-        for idx in range(force.getNumBonds()):
-            original_parameters.append(force.getBondParameters(idx))
-        self._original_parameters = original_parameters
-        
-    def _add_unbrella(self):
-        force = self.centered_normal.force()
-        self.system.addForce(force)
-        
-    def restore(self):
-        force = self.system.getForce(0)
-        for idx, parameters in enumerate(self._original_parameters):
-            force.setBondParameters(idx, *parameters)
-        force.updateParametersInContext(self.context)
-        force = self.system.getForce(1)
-        force.setGlobalParameterDefaultValue(0, 0.0)
-        
+                        
     def sample(self):
         sampled = {}
         log_p = 0.0
@@ -115,91 +127,53 @@ class Policy(torch.nn.Module):
             lamb = schedule(time, gamma, coefficient)
             coefficients[key] = lamb
         return coefficients
-    
-    def perturb(self, coefficients):
-        force = self.system.getForce(0)
-        for idx in range(force.getNumBonds()):
-            parameters = [coefficients["k2"], coefficients["k4"], 4.0]
-            _, __, original_parameters = self._original_parameters[idx]
-            parameters = [original_parameters[0] * parameters[0], original_parameters[1] * parameters[1], 4.0]
-            force.setBondParameters(idx, 2*idx, 2*idx+1, parameters)
-        force.updateParametersInContext(self.context)
-        force = self.system.getForce(1)
-        force.setGlobalParameterDefaultValue(0, 1-coefficients["k_gaussian"])
-        force.updateParametersInContext(self.context)
+            
+    @staticmethod
+    def integration(positions, coefficients, system, original_parameters):
+        add_umbrella(system)
+        integrator = OverdampedLangevinIntegrator(100.0, 100.0, 1e-2)
+        context = mm.Context(system, integrator, mm.Platform.getPlatformByName("CPU"))
+        integrator.ratio = 0.0
+        context.setPositions(positions)
+        for coefficient in coefficients:
+            perturb(original_parameters, coefficient, system, context)
+            integrator.step(100)
+        # restore(system, original_parameters)
+        energy = context.getState(getEnergy=True).getPotentialEnergy()._value
+        ratio = integrator.ratio
+        return energy, ratio
+        
     
     def trial(self):
-        sampled, log_p = self.sample()
-        self.integrator.ratio = 0.0
-        initial_position = np.random.randn(2, 3)
-        self.context.setPositions(initial_position)
-        self.restore()
-        for step in range(100):
-            time = torch.tensor(step / 100.0)
-            coefficients = self.get_coefficient(time, sampled)
-            self.perturb(coefficients)
-            self.integrator.step(100)
-        self.restore()
-        state = self.context.getState(getEnergy=True, getPositions=True)
-        energy = state.getPotentialEnergy()._value
-        positions = state.getPositions(asNumpy=True)._value
-        # distance = np.linalg.norm(positions[0] - positions[1])
-        initial_energy = self.centered_normal.log_prob(initial_position).sum()
-        log_w = -torch.tensor(energy) + initial_energy + self.integrator.ratio
-        # print(energy, initial_energy.item(), self.integrator.ratio)
-        if np.isnan(log_w):
-            log_w = torch.tensor(0.0)
-        return log_w, log_p
-        
-        
+        samples, log_p_batch = zip(*[self.sample() for _ in range(self.batch_size)])
+        coefficients = [[self.get_coefficient(time, sampled) for time in torch.linspace(0, 1, 10)] for sampled in samples]
+        positions = [self.centered_normal.sample().detach().numpy() for _ in range(self.batch_size)]
+        systems = [copy.deepcopy(self.system) for _ in range(self.batch_size)]
+        energies, ratios = zip(*Pool().starmap(self.integration, zip(positions, coefficients, systems, [_get_original(system) for system in systems])))
+        energies_initial = [self.centered_normal.log_prob(torch.tensor(position)).sum() for position in positions]
+        log_w_batch = [-energy + ratio + energy_initial for energy, ratio, energy_initial in zip(energies, ratios, energies_initial)]
+        return torch.stack(log_w_batch), torch.stack(log_p_batch)
+            
+
 def run():
     BATCH_SIZE = 10
-    integrator = OverdampedLangevinIntegrator(100.0, 100.0, 1e-2)
     system = dw2()
-    policy = Policy(system, integrator)
+    policy = Policy(system, BATCH_SIZE)
     
     optimizer = torch.optim.Adam(
         policy.parameters(),
         lr=1e-2,
     )
     
-    counter = 0
-    log_w, log_p = [], []
-    for _ in range(10000000):
-        _log_w, _log_p = policy.trial()
-        log_w.append(_log_w)
-        log_p.append(_log_p)
-        counter += 1
-        
-        if counter % BATCH_SIZE == 0:
-            log_w = torch.stack(log_w)
-            log_p = torch.stack(log_p)
-            ess = 1 / (log_w.softmax(0) ** 2).sum()
-            loss = -(log_w.exp() * log_p).mean()
-            loss.backward()
-            log_w, log_p = [], []
-            optimizer.step()
-            optimizer.zero_grad()
-            counter = 0
-            print(loss.item(), ess.item())
+    for _ in range(10000000000):
+        optimizer.zero_grad()
+        log_w, log_p = policy.trial()
+        loss = -(log_w.exp() * log_p).mean()
+        loss.backward()
         optimizer.step()
+        ess = 1 / (log_w.softmax(-1) ** 2).sum()
+        print(ess)
     
-    # for _ in range(100000):
-    #     optimizer.zero_grad()
-    #     log_w, log_p = policy.trial()
-    #     loss = -log_w.exp() * log_p
-    #     loss.backward()
-    #     print(log_w.exp().mean().item())
-    #     optimizer.step()
-    
-    # distances = []
-    # for _ in range(100):
-    #     _, __, distance = policy.trial()
-    #     distances.append(distance)
-    # import matplotlib.pyplot as plt
-    # import seaborn as sns
-    # sns.kdeplot(distances)
-    # plt.savefig("distance.png")
         
     
     
